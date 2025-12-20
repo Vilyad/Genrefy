3b6e942ef0e1871492429833b50688e1ae367783
Vilyad <lyscovvlad@gmail.com>
2025-12-20 21:13:40 +0500

    feat: create core models (Genre, Artist, Track)

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    spotify_genre_id = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='subgenres')
+
+    def __str__(self):
+        return self.name
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+    spotify_id = models.CharField(max_length=100, unique=True)
+    genres = models.ManyToManyField(Genre, related_name='artists')
+
+    def __str__(self):
+        return self.name
+
+
+class Track(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='tracks')
+    spotify_id = models.CharField(max_length=100, unique=True)
+    preview_url = models.URLField(max_length=500, blank=True)
+    audio_features = models.JSONField(default=dict, blank=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.artist.name}"
