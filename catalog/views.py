cb831d18860a0e310a98bf858b83764b2db9a4eb
Vilyad <lyscovvlad@gmail.com>
2025-12-21 01:30:17 +0500

    fix: fixed errors in templates and improved genre_list, view

--- catalog/views.py
+++ catalog/views.py
@@ -3,11 +3,14 @@ from .models import Genre, Artist, Track
 
 
 def genre_list(request):
-    genres = Genre.objects.all().prefetch_related('artists__tracks')
+    genres = Genre.objects.all()
 
     for genre in genres:
         track_count = Track.objects.filter(artist__genres=genre).count()
         genre.track_count = track_count
+
+        artist_count = Artist.objects.filter(genres=genre).count()
+        genre.artist_count = artist_count
 
     return render(request, 'catalog/genre_list.html', {
         'genres': genres,
