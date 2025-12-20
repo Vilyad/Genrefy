76ebf95a400949cd7626cf684757356604f00149
Vilyad <lyscovvlad@gmail.com>
2025-12-20 21:27:49 +0500

    feat: configure Django admin for models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Genre, Artist, Track
 
-# Register your models here.
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent')
+    search_fields = ('name',)
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'spotify_id')
+    search_fields = ('name',)
+    filter_horizontal = ('genres',)
+
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ('title', 'artist', 'spotify_id')
+    list_filter = ('artist__genres',)
+    search_fields = ('title', 'artist__name')
