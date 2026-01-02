7697662dab6b2489892d8dd21c307fb11fc5f762
Vilyad <lyscovvlad@gmail.com>
2026-01-03 01:08:03 +0500

    fix: fixed service initialization

--- catalog/services/__init__.py
+++ catalog/services/__init__.py
@@ -1,12 +1,7 @@
 from .lastfm_service import LastFMService
 from .visualization import VisualizationService
 
-lastfm_service = LastFMService()
-visualization_service = VisualizationService()
-
 __all__ = [
     'LastFMService',
     'VisualizationService',
-    'lastfm_service',
-    'visualization_service'
 ]
