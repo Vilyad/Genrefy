4d53900ba5e4e185f715e986ed3203bda1b24682
Vilyad <lyscovvlad@gmail.com>
2026-01-03 16:59:33 +0500

    update: Update services package to include new catalog and analytics services

--- catalog/services/__init__.py
+++ catalog/services/__init__.py
@@ -1,7 +1,13 @@
+from .base_service import BaseAPIService
 from .lastfm_service import LastFMService
 from .visualization import VisualizationService
+from .catalog_service import CatalogService
+from .analytics_service import AnalyticsService
 
 __all__ = [
+    'BaseAPIService',
     'LastFMService',
     'VisualizationService',
+    'CatalogService',
+    'AnalyticsService'
 ]
