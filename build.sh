be4cc43ffbda8c47c70df5a75f5a6d0299c63b4c
Vilyad <lyscovvlad@gmail.com>
2026-01-12 00:16:42 +0500

    fix: correct build.sh append migrations command

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
+python manage.py migrate --no-input
+
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+python manage.py load_demo.py
